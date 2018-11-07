7460fe20478356ede149231a489f65cd587c195a
krawal19 <kapilrawal1995@gmail.com>
2018-11-06 23:46:08 -0500

    Added functionalities in the talker file for providing a service to modify string

--- src/talker.cpp
+++ src/talker.cpp
@@ -41,16 +41,34 @@
  *
  *  @section   DESCRIPTION
  *
- *  This program publishes the message 'You are the best' on the topic chatter
+ *  This program publishes the message on the topic chatter
  *
  */
-
 #include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "beginner_tutorials/serviceFile.h"
+
+/// Initialize the base string to print
+extern std::string msgSent = "Did you pass 808x? ";
 
 /**
- *  @brief   This tutorial demonstrates simple sending of messages over the ROS system.
+ *  @brief changes base string of talker
+ *  
+ *  @param req requesting new string from service client
+ *
+ *  @param res response of the service from client
+ *
+ *  @return true service function executed
+ */
+bool change(beginner_tutorials::changeBaseString::Request &req,
+            const beginner_tutorials::changeBaseString::Response &res) {
+  msgSent = msgSent + req.newString;
+  return true;
+}
+
+/**
+ *  @brief   This tutorial demonstrates simple Receving of messages over the ROS system.
  *
  *  @param   argc  Count of arguments given on terminal
  *
@@ -59,47 +77,66 @@
  *  @return  returns nothing
  */
 int main(int argc, char **argv) {
-  /// Initialize function for the node, for name remapping and
-  /// to perform any ROS arguments
+  /// Initializes the talker node
   ros::init(argc, argv, "talker");
-  ///  Create a instance of NodeHandle
-  ros::NodeHandle n;
+  /// Initializing frequency
+  std::string result;
+  /// Checking if we have two arguments
+  if (argc == 2) {
+    /// recording the argument passed
+    ROS_DEBUG_STREAM("Argument is " << argv[1]);
+    /// converts string to integer
+    result = argv[1];
+    if (result.empty()) {
+      ROS_ERROR_STREAM(
+          "No was given, using default string");
+       result = "Didn't took the course";
+    }
 
-  ///  Create a Publisher node and define topic named chatter
-  ///  Publish message to topic with queue size of 1000
+  } else {
+    /// shuts down the node when no argument given
+    ROS_FATAL_STREAM("Not applicable for this student");
+    ros::shutdown();
+    return 1;
+  }
+  /// Creating a handle to process
+  ros::NodeHandle n;
+  
+  /// defines the topic and type of message on which the
+  /// publisher is going to publish data
   ros::Publisher chatter_pub = n.advertise < std_msgs::String
       > ("chatter", 1000);
-
-  ///  run loop at 10Hz rate
+  
+  /// defining service server
+  ros::ServiceServer server = n.advertiseService("changeString", change);
+  
+  /// Rate of loop running.
   ros::Rate loop_rate(10);
-
-  ///  count of sent message
+  
+  /// Number of messages that have been sent.
   int count = 0;
-
-  ///  loop until node is shutdown
   while (ros::ok()) {
-    /// create String message
+
+    /// Creating a message object to stuff data and then publish it.
     std_msgs::String msg;
     std::stringstream ss;
-    ss << "Your are the best" << count;
-
-    ///  store custom message string
+    ss << msgSent + result << count;
     msg.data = ss.str();
 
-    ///  display message
+    /// print data on terminal
     ROS_INFO("%s", msg.data.c_str());
 
-    ///  using publisher object to publish messages
+    /// message output as broadcast
     chatter_pub.publish(msg);
 
-    ///  asks ROS to execute all of the pending
-    ///  callbacks from all node's subscriptions
+    /// for subscriber node
     ros::spinOnce();
 
-    ///  Sleep the publishing till next iteration
+    /// when not publishing in sleep mode to match the publish rate
     loop_rate.sleep();
     ++count;
+    ROS_DEBUG_STREAM("Argument is " << count);
   }
+
   return 0;
 }
-
